91cc9d4bee722ebf1c44971d29704774b21dfa5a
ACure8 <alexisme952@gmail.com>
2026-03-20 11:52:23 +1300

    Basic start plain

---
+++ runflask.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, send_from_directory
+
+website = Flask(__name__, template_folder="templates")
+
+@website.route("/")
+def main_page():
+    return render_template("practicesql.html")
+
+if __name__ == "__main__":
+    print("\n\033[1;95m- LOADING... -\033[0m\n")
+    website.run(debug=True, port=5000)
