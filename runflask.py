b590210885da8c08caa3790ea9930777c53b6e2c
ACure8 <alexisme952@gmail.com>
2026-04-18 14:48:41 +0000

    huge changes: -buttons/links are working and better looking. -since id's (id="") have higher priority than class (class="") I have changed it so that before and after effects are properly working and easy to work with. -added connections in flask so links finally work(with some additions that'll be used in the future). -phone size or such screen size effects are still a work in progress. -related to content stated above: when screen reaches a certain size the nav buttons will disappear and replacing it will be a menu bar, currently not interactable(ass in nothing will happen if pressed). -images are not working on my part(likely because of "fluffy doodle") -unlikely to use SQL.js at this time, but might be used in the future for proper organization

--- runflask.py
+++ runflask.py
@@ -3,8 +3,24 @@ from flask import Flask, render_template, send_from_directory
 website = Flask(__name__, template_folder="templates")
 
 @website.route("/")
-def main_page():
+def home_page():
     return render_template("Wowfoods.html")
+
+@website.route("/menu")
+def menu_page():
+    return render_template("Menu.html")
+
+@website.route("/cart")
+def cart_page():
+    return render_template("Cart.html")
+
+@website.route("/account")
+def signin_page():
+    return render_template("Signin.html")
+
+@website.route('/user/<username>')
+def show_user(username):
+    return f'Hello {username} ! please wait while we send you back'
 
 if __name__ == "__main__":
     print("\n\033[1;95m- LOADING... -\033[0m\n")
