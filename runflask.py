68de3a603e3dc805cf7dc3f14117de024811a6cb
ACure8 <alexisme952@gmail.com>
2026-05-12 21:44:28 +0000

    removed customers and replaced with a fully finished database(wowfoods.db)

--- runflask.py
+++ runflask.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, sqlite3, easygui as eg
 
 website = Flask(__name__, template_folder="templates")
 
